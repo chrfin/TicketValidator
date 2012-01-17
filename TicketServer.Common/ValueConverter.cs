32d36fead3f2d0f0a81ef9df7deab1b7da33d540
Christoph Fink <chrfin@users.noreply.github.com>
2012-01-17 16:54:01 +0000

    minor UI improvement

--- TicketServer.Common/ValueConverter.cs
+++ TicketServer.Common/ValueConverter.cs
@@ -90,4 +90,43 @@ namespace TicketServer.Common
 		#endregion
 	}
 
+	/// <summary>
+	/// Retruns the file name of a path.
+	/// </summary>
+	public class PathToFileNameConverter : IValueConverter
+	{
+		#region IValueConverter Members
+
+		/// <summary>
+		/// Converts a value.
+		/// </summary>
+		/// <param name="value">The value produced by the binding source.</param>
+		/// <param name="targetType">The type of the binding target property.</param>
+		/// <param name="parameter">The converter parameter to use.</param>
+		/// <param name="culture">The culture to use in the converter.</param>
+		/// <returns>
+		/// A converted value. If the method returns null, the valid null value is used.
+		/// </returns>
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return System.IO.Path.GetFileName(value.ToString());
+		}
+
+		/// <summary>
+		/// Converts a value.
+		/// </summary>
+		/// <param name="value">The value that is produced by the binding target.</param>
+		/// <param name="targetType">The type to convert to.</param>
+		/// <param name="parameter">The converter parameter to use.</param>
+		/// <param name="culture">The culture to use in the converter.</param>
+		/// <returns>
+		/// A converted value. If the method returns null, the valid null value is used.
+		/// </returns>
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
 }
