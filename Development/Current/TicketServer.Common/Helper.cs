cd8a056e229ee09d3e8ef04dc48b334bcfffd127
CFI_cp <SND\CFI_cp>
2012-01-23 17:41:52 +0000

    WIP

---
+++ Development/Current/TicketServer.Common/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace TicketServer.Common
+{
+	/// <summary>
+	/// Container for several helper methods
+	/// </summary>
+	public static class Helper
+	{
+	}
+}
